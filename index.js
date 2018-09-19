54f17ef51fc4c6b0067746253018fb3a4a8d9d09
Vaishal Gandhi <vaishal48@gmail.com>
2018-09-20 00:42:25 +0530

    - broken build - added .editorconfig - changes related to es6 and class based structure

--- index.js
+++ index.js
@@ -5,19 +5,35 @@ require("./server/globals");
 const app = require(`${__dirServer}`);
 
 // grab the defined port from config file
-const { port, } = require(`${__dirServer}config/`);
+const { port } = require(`${__dirServer}config/`);
 
-// Listing our app on specified port
-app.listen(port);
-console.log("listening on http://localhost:" + port);
+class App
+{
+	constructor () {
+		this.port = port;
+		this.app = app;
+		this.initalizeApplication();
+		this.handleProcessEvent();
+	}
 
-// Handle Exit Event
-process
-// Handle normal exits
-    .on("exit", (code) => {
-        process.exit(code);
-    })
-// Handle CTRL+C
-    .on("SIGINT", () => {
-        process.exit(0);
-    });
+	initalizeApplication() {
+		// Listing our app on specified port
+		this.app.listen(this.port);
+		console.log("listening on http://localhost:" + this.port);
+	}
+
+	handleProcessEvent() {
+		// Handle Exit Event
+		process
+			// Handle normal exits
+		    .on("exit", (code) => {
+		        process.exit(code);
+		    })
+			// Handle CTRL+C
+		    .on("SIGINT", () => {
+		        process.exit(0);
+		    });
+	}
+}
+
+module.exports = new App()
--- server/middleware/appMiddlware.js
+++ server/middleware/appMiddlware.js
@@ -5,24 +5,44 @@ const helmet = require("helmet");
 const override = require("method-override");
 
 // setup global middleware here
-module.exports = function(app) {
-    // Limiting the number of request
-    const limiter = expressLimiter(app);
-    limiter(app, {
-        total: 60, // Allowed Number pf request before limits get expired
-        expire: 1000 * 60, // Limits gets expired in defined miliseconds
-    });
+export default class ApplicationMiddleware
+{
+    constructor(app) {
+        this.app = app;
+        this.expressLimiter();
+        this.bodyParser();
+        this.cors();
+        this.helmet();
+        this.override();
+    }
 
-    // Parse incoming request data
-    app.use(bodyParser.urlencoded({ extended: true, }));
-    app.use(bodyParser.json());
+    expressLimiter() {
+        // Limiting the number of request
+        const limiter = expressLimiter(this.app);
+        limiter(app, {
+            total: 60, // Allowed Number pf request before limits get expired
+            expire: 1000 * 60, // Limits gets expired in defined miliseconds
+        });
+    }
 
-    // Allowing cross-origin request by allowing preflight-check request
-    app.use(cors());
+    bodyParser() {
+        // Parse incoming request data
+        this.app.use(bodyParser.urlencoded({ extended: true }));
+        this.app.use(bodyParser.json());
+    }
 
-    // Securing express app by setting various HTTP headers
-    app.use(helmet());
+    cors() {
+        // Allowing cross-origin request by allowing preflight-check request
+        this.app.use(cross-origin());
+    }
 
-    // Use HTTP verbs such as PUT or DELETE in places where the client doesn't support it
-    app.use(override());
-};
+    helmet() {
+        // Securing express app by setting various HTTP headers
+        this.app.use(helmet());
+    }
+
+    override() {
+        // Use HTTP verbs such as PUT or DELETE in places where the client doesn't support it
+        this.app.use(override());
+    }
+}
